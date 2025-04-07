08d78ebbd91956af2a16355de20bec87c5d9f0c3
Stilian Mihnev <stilianmh@abv.bg>
2025-04-07 16:47:37 +0300

    Search bar errors fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, CssBaseline, Box, Container } from '@mui/material';
 import { theme } from './theme';
 import Navbar from './components/Navbar';
-import AppRoutes from './routes';
 import { AuthProvider } from './contexts/AuthContext';
 import { AnalyticsProvider } from './components/AnalyticsProvider';
 import { useTheme } from '@mui/material';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import CreateListing from './pages/CreateListing';
 import ListingDetails from './pages/ListingDetails';
+import EditListing from './pages/EditListing';
+import Search from './pages/Search';
 
 const AppContent = () => {
   const theme = useTheme();
@@ -51,8 +57,14 @@ const AppContent = () => {
           }}
         >
           <Routes>
-            <Route path="/" element={<AppRoutes />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-listing" element={<CreateListing />} />
             <Route path="/listing/:id" element={<ListingDetails />} />
+            <Route path="/edit-listing/:id" element={<EditListing />} />
+            <Route path="/search" element={<Search />} />
           </Routes>
         </Container>
       </Box>
